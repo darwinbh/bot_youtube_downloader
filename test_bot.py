2f8c1516f7e7e30719f88d0614ddcf57c99e41d3
darwin <darwindon2004@gmail.com>
2025-10-18 16:51:06 +0100

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,15 +1,1 @@
-from telegram import Bot
 
-TOKEN = "8121555720:AAHOZ3WNi5H7N0lrNrK4dyCoY_wTiaFpGzU"
-
-print("🔹 بدأ التحقق من التوكن...")
-
-try:
-    bot = Bot(token=TOKEN)
-    me = bot.get_me()
-    print("✅ التوكن صالح!")
-    print(me)
-except Exception as e:
-    print("❌ خطأ:", e)
-
-input("اضغط Enter للخروج...")
